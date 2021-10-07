5370ff57f2bfb2467967acb780b4d285dea8715e
francamaradev <9179406-francamara1@users.noreply.gitlab.com>
2021-10-07 18:17:37 +0200

    feat: added chicha command

---
+++ src/commands/music/chicha.js
@@ -0,0 +1,12 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('chicha')
+    .setDescription('Muy temprano pero que rica chichita'),
+  async execute(interaction) {
+    await interaction.reply(
+      'Nunca es tarde para una _chichita_ \n https://open.spotify.com/playlist/0apSD1XZsZ86s81wpHeuYt?si=c197c462686d4192'
+    )
+  },
+}
