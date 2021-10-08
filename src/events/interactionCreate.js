32bf95940dc7ee4e521294a459e344c88edd5018
francamaradev <9179406-francamara1@users.noreply.gitlab.com>
2021-10-08 12:39:56 +0200

    feat: log in console every command executed

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -1,9 +1,17 @@
+const colors = require('colors')
+
 module.exports = {
   name: 'interactionCreate',
   async execute(interaction, client) {
     if (!interaction.isCommand()) return
 
     const command = client.commands.get(interaction.commandName)
+
+    console.log(
+      colors.white(
+        `COMMAND: "/${command.data.name}" EXECUTED BY ${interaction.user.tag}`
+      )
+    )
 
     if (!command) return
 
@@ -12,9 +20,11 @@ module.exports = {
     } catch (error) {
       console.error(error)
       await interaction.reply({
-        content: 'There was an error while executing this command!',
+        content:
+          'There was an error while executing this command! (If problem persists, please contact @francamaraph#9089',
         ephemeral: true,
       })
+      console.log(colors.red(`Error executing "/${command.data.name}"`))
     }
   },
 }
