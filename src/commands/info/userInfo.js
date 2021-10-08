1e475492f353cdb247568b32311e1de8ac0de117
francamaradev <9179406-francamara1@users.noreply.gitlab.com>
2021-10-08 13:51:21 +0200

    feat: added date created, picture and more stuff to /userinfo

--- src/commands/info/userInfo.js
+++ src/commands/info/userInfo.js
@@ -1,20 +1,44 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
-const { Client, Collection, Intents, MessageEmbed } = require('discord.js')
-// const client = new Client({ intents: [Intents.FLAGS.GUILDS] })
+const { MessageEmbed } = require('discord.js')
+const moment = require('moment')
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('userinfo')
     .setDescription('Info about the user who summoned this command'),
   async execute(interaction) {
+    // const now = moment(new Date())
+    const createdDate = moment(interaction.user.createdTimestamp).format(
+      'DD/MM/YYYY'
+    )
+    // const days = createdDate.diff(now, 'days')
+    // console.log(Date.now())
     const userInfo = {
-      userName: interaction.user.tag,
+      name: interaction.user.username,
+      tag: interaction.user.tag,
+      accountAge: createdDate,
+      avatar: interaction.user.avatarURL(),
     }
+
     interaction.reply({
       embeds: [
         new MessageEmbed()
           .setColor('#C219D8')
-          .setTitle(`Username: ${userInfo.userName}`),
+          // .setThumbnail(`${userInfo.avatar}`)
+          .setTitle(`${userInfo.name}`)
+          .addFields(
+            {
+              name: 'Tag',
+              value: `${userInfo.tag}`,
+              inline: true,
+            },
+            {
+              name: 'Created at',
+              value: `${userInfo.accountAge}`,
+              inline: true,
+            }
+          )
+          .setImage(`${userInfo.avatar}`),
       ],
     })
   },
