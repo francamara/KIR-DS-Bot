8dc3771397257ce7b4f7d0876e1b40fc6792e9b8
francamaradev <9179406-francamara1@users.noreply.gitlab.com>
2021-10-17 17:00:06 +0200

    feat: [WIP] play pop corn andino

--- src/commands/music/chicha.js
+++ src/commands/music/chicha.js
@@ -1,12 +1,55 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
+const {
+  joinVoiceChannel,
+  createAudioResource,
+  VoiceConnection,
+  getVoiceConnection,
+} = require('@discordjs/voice')
+const { createAudioPlayer } = require('@discordjs/voice')
+const { generateDependencyReport } = require('@discordjs/voice')
+// const {  } = require('@discordjs/voice');
+
+// DONE : Connect bot to a voice channel
+
+// TODO : Separate either if the content sent in the slash command is youtube, spotify, string or whatever
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('chicha')
-    .setDescription('Muy temprano pero que rica chichita'),
+    .setDescription('Pop Corn Andino'),
+
   async execute(interaction) {
-    interaction.reply(
-      'Nunca es tarde para una _chichita_ \n https://open.spotify.com/playlist/0apSD1XZsZ86s81wpHeuYt?si=c197c462686d4192'
-    )
+    /* Gets the voice channel where the member is in. If the member isn't in any, return. */
+    const voiceChannel = interaction.member.voice.channel
+    // console.log(voiceChannel)
+    if (!voiceChannel)
+      return interaction.reply({
+        content: 'You need to be in a voice channel!',
+        ephemeral: true,
+      })
+    if (voiceChannel) {
+      // const audioSource = createAudioResource('../../data/popCornAndino.mp4')
+
+      joinVoiceChannel({
+        channelId: voiceChannel.id,
+        guildId: voiceChannel.guild.id,
+        adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+      })
+
+      const player = createAudioPlayer()
+
+      const audioSource = createAudioResource(
+        'https://streams.ilovemusic.de/iloveradio109.mp3'
+      )
+
+      player.play(audioSource)
+
+      // console.log(generateDependencyReport())
+
+      return interaction.reply({
+        content: 'Ay que rica chichita',
+        // ephemeral: true,
+      })
+    }
   },
 }
