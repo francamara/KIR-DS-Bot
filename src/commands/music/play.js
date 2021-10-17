91bfd855e6e6d74a90dcd0bb23710af65af28bb9
francamaradev <9179406-francamara1@users.noreply.gitlab.com>
2021-10-17 17:00:25 +0200

    feat: [WIP] Using dependency plays music from mp3 file

--- src/commands/music/play.js
+++ src/commands/music/play.js
@@ -1,40 +1,28 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
-const { joinVoiceChannel } = require('@discordjs/voice')
-
-// DONE : Connect bot to a voice channel
-
-// TODO : Separate either if the content sent in the slash command is youtube, spotify, string or whatever
+const music = require('@koenie06/discord.js-music')
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('play')
-    .setDescription('Plays a song in the voice channel')
-    .addStringOption((string) =>
-      string
-        .setName('song')
-        .setDescription('Play a given song name/URL in the voice channel')
-        .setRequired(true)
-    ),
+    .setDescription('Plays a song in the voice channel'),
+
   async execute(interaction) {
     /* This will get the song that has been provided */
-    const song = interaction.options.getString('song')
-
-    console.log(song)
+    const song = '../../data/popCornAndino.mp4'
 
     /* Gets the voice channel where the member is in. If the member isn't in any, return. */
     const voiceChannel = interaction.member.voice.channel
-    // console.log(voiceChannel)
     if (!voiceChannel)
       return interaction.reply({
         content: 'You need to be in a voice channel!',
         ephemeral: true,
       })
-    if (voiceChannel) {
-      joinVoiceChannel({
-        channelId: voiceChannel.id,
-        guildId: voiceChannel.guild.id,
-        adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-      })
-    }
+
+    /* Get more info about how the play command works at https://npmjs.com/package/@koenie06/discord.js-music#play */
+    music.play({
+      interaction: interaction,
+      channel: voiceChannel,
+      song: song,
+    })
   },
 }
