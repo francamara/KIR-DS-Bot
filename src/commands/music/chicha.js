46a4a1f8aab76dd49fcf471972f380ddf583493c
francamaradev <9179406-francamara1@users.noreply.gitlab.com>
2021-12-03 22:05:23 -0300

    refactor: changed logic of endpoint

--- src/commands/music/chicha.js
+++ src/commands/music/chicha.js
@@ -1,55 +1,30 @@
 const { SlashCommandBuilder } = require('@discordjs/builders')
-const {
-  joinVoiceChannel,
-  createAudioResource,
-  VoiceConnection,
-  getVoiceConnection,
-} = require('@discordjs/voice')
-const { createAudioPlayer } = require('@discordjs/voice')
-const { generateDependencyReport } = require('@discordjs/voice')
-// const {  } = require('@discordjs/voice');
-
-// DONE : Connect bot to a voice channel
-
-// TODO : Separate either if the content sent in the slash command is youtube, spotify, string or whatever
+const music = require('@koenie06/discord.js-music')
 
 module.exports = {
   data: new SlashCommandBuilder()
     .setName('chicha')
-    .setDescription('Pop Corn Andino'),
+    .setDescription('CHICHAAA CHICHA DE JORA'),
 
   async execute(interaction) {
+    /* This will get the song that has been provided */
+    const song = '../../data/popCornAndino.mp4'
+
     /* Gets the voice channel where the member is in. If the member isn't in any, return. */
     const voiceChannel = interaction.member.voice.channel
-    // console.log(voiceChannel)
     if (!voiceChannel)
       return interaction.reply({
         content: 'You need to be in a voice channel!',
         ephemeral: true,
       })
-    if (voiceChannel) {
-      // const audioSource = createAudioResource('../../data/popCornAndino.mp4')
 
-      joinVoiceChannel({
-        channelId: voiceChannel.id,
-        guildId: voiceChannel.guild.id,
-        adapterCreator: voiceChannel.guild.voiceAdapterCreator,
-      })
+    /* Get more info about how the play command works at https://npmjs.com/package/@koenie06/discord.js-music#play */
+    music.play({
+      interaction: interaction,
+      channel: voiceChannel,
+      song: song,
+    })
 
-      const player = createAudioPlayer()
-
-      const audioSource = createAudioResource(
-        'https://streams.ilovemusic.de/iloveradio109.mp3'
-      )
-
-      player.play(audioSource)
-
-      // console.log(generateDependencyReport())
-
-      return interaction.reply({
-        content: 'Ay que rica chichita',
-        // ephemeral: true,
-      })
-    }
+    return interaction.reply('Chicha en 8 bitardos')
   },
 }
